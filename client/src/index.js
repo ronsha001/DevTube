d16f4c30a4a1411592e8b783ffdeb7daa56f5fff
ronsha001 <ronsh0111@gmail.com>
2022-09-05 19:25:47 +0300

    scrollbar design

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './App';
 import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
